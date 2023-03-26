737569a7cdee6079781acdbcf7ea09521853e2e4
Александр Пономаренко <ponomarenko.100299@gmail.com>
2023-03-26 17:47:17 +0300

    administration django

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class Product(models.Model):
     class Meta:
         ordering = ["-name"]
-        # db_table = "tech_products"
-        # verbose_name_plural = "products"
 
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
@@ -14,6 +12,15 @@ class Product(models.Model):
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+
+    @property
+    def description_short(self) -> str:
+        if len(self.description) < 48:
+            return self.description
+        return self.description[:48] + "..."
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}, name={self.name!r})"
 
 
 class Order(models.Model):
