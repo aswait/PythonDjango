395b91a0ca94be3423b3dbbef216f9680808f5c7
Александр Пономаренко <ponomarenko.100299@gmail.com>
2023-04-03 12:02:50 +0300

    authentication

---
+++ mysite/myauth/urls.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from .views import (
+    get_cookie_view,
+    set_cookie_view,
+    get_session_view,
+    set_session_view,
+    logout_view,
+    MyLogoutView
+)
+
+
+app_name = "myauth"
+
+urlpatterns = [
+    path(
+        'login/',
+        LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name='login'
+    ),
+    # path('logout/', logout_view, name="logout"),
+    path('logout/', MyLogoutView.as_view(), name="logout"),
+    path('cookie/get', get_cookie_view, name="cookie-get"),
+    path('cookie/set', set_cookie_view, name="cookie-set"),
+    path('session/get', get_session_view, name="session-get"),
+    path('session/set', set_session_view, name="session-set"),
+]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'shopapp.apps.ShopappConfig',
     'requestdataapp.apps.RequestdataappConfig',
+    'myauth.apps.MyauthConfig'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admin/'
